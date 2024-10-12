1b99ecb99b81a8da632b20292e58213b3b6a0d41
TodTheMatsu <gokudollmeanak@gmail.com>
2024-10-12 11:01:56 +0700

    a bit of changes in styles and new method of adding tasks

--- src/TaskBoard.jsx
+++ src/TaskBoard.jsx
@@ -1,38 +1,84 @@
-import { useContext } from 'react';
+import { useState } from 'react';
 import { Droppable, Draggable } from 'react-beautiful-dnd';
 import TaskCard from './TaskCard.jsx';
 import './index.css';
 
 console.log('TaskBoard.jsx loaded');
 
-function TaskBoard({ tasks, boardId, deleteTask, setDetailsVisbility }) {
+function TaskBoard({ tasks, boardId, deleteTask, setDetailsVisbility, setTasks, boards}) {
+  const [taskText, setTaskText] = useState("");
+
+  const handleInputChange = (e) => {
+    setTaskText(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!taskText) return;
+
+    // Create a new task object
+    const newTask = {
+      name: taskText,
+      note: "",
+    };
+
+    const updatedBoardTasks = [...boards[boardId], newTask];
+
+    // Update the boardTasks state with the new task for board 1
+    setTasks({
+      ...boards,
+      [boardId]: updatedBoardTasks
+    });
+
+    setTaskText("");
+  };
+
   return (
     <div>
-    <Droppable droppableId={`Board-${boardId}`} type='group'>
-      {(provided) => (
-        <div
-          ref={provided.innerRef}
-          {...provided.droppableProps}
-          className="w-60 min-h-[5vh]  flex-grow-0 h-min mt-5 mx-2 border-4 border-gray-300 bg-gray-200 shadow-2xl pt-0.5 px-3 rounded-lg"
-        >
-          {tasks && tasks.map((task, index) => (
-            <Draggable key={index} draggableId={`draggable-${boardId}-${index}`} index={index}>
-              {(provided, snapshot) => (
-                <div
-                  ref={provided.innerRef}
-                  {...provided.draggableProps}
-                  {...provided.dragHandleProps}
-                  className="draggable"
-                >
-                  <TaskCard task={task} provided={provided} snapshot={snapshot} isDragging={snapshot.isDragging} deleteTask={() => deleteTask(boardId, index)} setDetailsVisbility={setDetailsVisbility} />
-                </div>
-              )}
-            </Draggable>
-          ))}
-          {provided.placeholder}
-        </div>
-      )}
-    </Droppable>
+      <Droppable droppableId={`Board-${boardId}`} type='group'>
+        {(provided) => (
+          <div
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            className="w-60 min-h-[5vh] flex-grow-0 h-min mt-5 mx-2 border-4 border-gray-300 bg-gray-200 shadow-2xl pt-0.5 px-3 rounded-lg"
+          >
+            {tasks && tasks.map((task, index) => (
+              <Draggable key={index} draggableId={`draggable-${boardId}-${index}`} index={index}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    className="draggable"
+                  >
+                    <TaskCard 
+                      task={task} 
+                      provided={provided} 
+                      snapshot={snapshot} 
+                      isDragging={snapshot.isDragging} 
+                      deleteTask={() => deleteTask(boardId, index)} 
+                      setDetailsVisbility={setDetailsVisbility} 
+                    />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+            <form onSubmit={handleSubmit} className="w-full">
+              <input 
+                className='w-full h-[3vh] mb-2 mt-2 border-4 bg-transparent shadow-2xl pt-0.5 rounded-lg text-center font-bold hover:bg-gray-300 hover:placeholder-gray-500 focus:outline-dashed focus:outline-gray-400 focus:placeholder-transparent hover:font-extrabold' 
+                placeholder='+ Add a task'
+                type="text"
+                value={taskText}
+                onChange={handleInputChange}
+                id='userInput'
+                autoComplete='off'
+              />
+              <button type="submit" className="hidden"></button>
+            </form>
+          </div>
+        )}
+      </Droppable>
     </div>
   );
 }
