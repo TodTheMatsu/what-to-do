8255fd2fddfbbb63cc55afbf2d4a905e5da186e8
TodTheMatsu <gokudollmeanak@gmail.com>
2024-12-23 13:02:51 +0700

    minor changes

--- server.js
+++ server.js
@@ -12,8 +12,6 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5000;
-
-
 mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log('MongoDB connected'))
   .catch(err => console.error('MongoDB connection error:', err));
